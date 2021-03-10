7ac26d7fef6bfa29c3a1bcf6626842e54271b7ad
ASUS <ASUS@DESKTOP-76S93E0>
2021-03-10 19:47:04 +0530

    [Lavanya] Added ferAddExpense Controller

---
+++ fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/model/Expense.java
@@ -0,0 +1,107 @@
+package com.bezkoder.spring.datajpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Expense {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer expenseid;
+
+	@Column
+	private String type;
+
+	@Column
+	private String date;
+
+	@Column
+	private float price;
+
+	@Column
+	private int numberOfItems;
+
+	@Column
+	private float total;
+
+	@Column
+	private String bywhom;
+	
+	public Expense() {
+
+	}
+
+	public Expense(String type, String date, float price, int numberOfItems, float total, String bywhom) {
+
+		this.type = type;
+		this.date = date;
+		this.price = price;
+		this.numberOfItems = numberOfItems;
+		this.total = total;
+		this.bywhom = bywhom;
+
+	}
+
+	public int getExpenseid() {
+		return expenseid;
+	}
+
+	public void setExpenseid(int expenseid) {
+		this.expenseid = expenseid;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public void setNumberOfItems(int numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public String getBywhom() {
+		return bywhom;
+	}
+
+	public void setBywhom(String bywhom) {
+		this.bywhom = bywhom;
+	}
+
+}
