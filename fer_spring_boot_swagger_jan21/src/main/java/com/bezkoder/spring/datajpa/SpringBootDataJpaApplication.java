c879d06d204d582a6f9c2db2b7844f0479174e04
SHIREESHA <SHIREESHA@LAPTOP-SNMEEU13>
2021-03-10 17:13:17 +0530

    Login

--- fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,8 +2,10 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+//@PropertySource("classpath:${CONFIG_MODE}/application.properties")
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
