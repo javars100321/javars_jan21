9bdfe633cacc7432fcff4d46cf655fa0c5e5eaa9
samsung <samsung@DESKTOP-SSCAN4I>
2021-03-10 20:58:26 +0530

    Included  edit expense method by Thanmai

--- fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
+++ fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
@@ -5,22 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bezkoder.spring.datajpa.model.Expense;
 import com.bezkoder.spring.datajpa.repository.ExpenseRepository;
 
 @RestController
 @RequestMapping("/api")
 public class FERController {
 
-	
-
-	
 	@Autowired
 	ExpenseRepository expenseRepository;
 
-	
 	@DeleteMapping("/expense/{expenseId}")
 	public ResponseEntity<HttpStatus> deleteExpense(@PathVariable("expenseId") int expenseId) {
 
@@ -32,6 +31,18 @@ public class FERController {
 		}
 
 	}
+
+	@PostMapping("/editexpense/{expenseId}")
+	public ResponseEntity<Expense> editExpense(@PathVariable int expenseId, @RequestBody Expense expense) {
+
+		try {
+
+			Expense _expense = expenseRepository.save(expense);
+
+			return new ResponseEntity<>(_expense, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
 }
-	
-	
