4b3e1dbd5d5fcada385b8e8a34cae762d2c11830
RamyaKrishna <RamyaKrishna@LAPTOP-GJBS59K5>
2021-03-10 23:13:59 +0530

    resetpassword by ramya

--- fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
+++ fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bezkoder.spring.datajpa.model.Expense;
@@ -26,7 +28,6 @@ public class FERController {
 	ExpenseRepository expenseRepository;
 	@Autowired
 	UserRepository userRepository;
-
 
 	@DeleteMapping("/expense/{expenseId}")
 	public ResponseEntity<HttpStatus> deleteExpense(@PathVariable("expenseId") int expenseId) {
@@ -53,25 +54,48 @@ public class FERController {
 		}
 
 	}
-	
+
 	@PutMapping("/updateUser/{userid}")
 	public ResponseEntity<User> updateUser(@PathVariable("userid") int id, @RequestBody User user) {
 		Optional<User> userData = userRepository.findById(id);
 
-			
-			if (userData.isPresent()) {
-				User _user=userRepository.save(
-						(new User(user.getFirstname(),user.getMiddlename(),
-						user.getLastname(),
-						user.getEmail(),
-						user.getUsername(),
-						user.getPassword(),
-						user.getMobile())));
+		if (userData.isPresent()) {
+			User _user = userRepository.save((new User(user.getFirstname(), user.getMiddlename(), user.getLastname(),
+					user.getEmail(), user.getUsername(), user.getPassword(), user.getMobile())));
 
 			return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
+
+	@GetMapping("/getUser/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
+		Optional<User> userData = userRepository.findById(id);
+
+		if (userData.isPresent()) {
+			return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		}
 	}
 
+	@PostMapping("/resetPassword/{userId}")
+	public ResponseEntity<String> resetPassword(@PathVariable("userId") int id, @RequestParam String currentpassword,
+			@RequestParam String newpassword) {
+
+		User _user = getUserById(id).getBody();
+
+		if ("currentPassword" != null) {
+			_user.setPassword(newpassword);
+			return new ResponseEntity<>((_user.getPassword() == newpassword ? "Valid User" : "Invalid User"),
+					HttpStatus.CREATED);
+
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		}
+	}
+
+}
