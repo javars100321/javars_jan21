bebf5d8f6fa55f7eca5abbdbcd31ec4961c08a74
Tejasri <Tejasri@DESKTOP-R5ABRU5>
2021-03-10 22:28:17 +0530

    UpdateUser committed by teja

--- fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
+++ fer_spring_boot_swagger_jan21/src/main/java/com/bezkoder/spring/datajpa/controller/FERController.java
@@ -1,17 +1,22 @@
 package com.bezkoder.spring.datajpa.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bezkoder.spring.datajpa.model.Expense;
+import com.bezkoder.spring.datajpa.model.User;
 import com.bezkoder.spring.datajpa.repository.ExpenseRepository;
+import com.bezkoder.spring.datajpa.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
@@ -19,6 +24,9 @@ public class FERController {
 
 	@Autowired
 	ExpenseRepository expenseRepository;
+	@Autowired
+	UserRepository userRepository;
+
 
 	@DeleteMapping("/expense/{expenseId}")
 	public ResponseEntity<HttpStatus> deleteExpense(@PathVariable("expenseId") int expenseId) {
@@ -45,4 +53,25 @@ public class FERController {
 		}
 
 	}
-}
+	
+	@PutMapping("/updateUser/{userid}")
+	public ResponseEntity<User> updateUser(@PathVariable("userid") int id, @RequestBody User user) {
+		Optional<User> userData = userRepository.findById(id);
+
+			
+			if (userData.isPresent()) {
+				User _user=userRepository.save(
+						(new User(user.getFirstname(),user.getMiddlename(),
+						user.getLastname(),
+						user.getEmail(),
+						user.getUsername(),
+						user.getPassword(),
+						user.getMobile())));
+
+			return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	}
+
